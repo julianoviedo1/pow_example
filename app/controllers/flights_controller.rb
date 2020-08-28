1112bd5497bce5e797408e75b25c2770782209e2
julianoviedo <julianoviedo1@gmail.com>
2020-08-28 16:58:36 -0300

    small fixes, front added

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,17 +1,20 @@
 class FlightsController < ApplicationController
   def index
     @places = Place.all
-    @flights = params ? fetch_flights : []
+    @flights = flight_params ? fetch_flights : []
+    @origin = Place.find_by(place_id: params["flight"]["originplace"]).place
+    @destination = Place.find_by(place_id: params["flight"]["destinationplace"]).place
   end
 
   private
 
   def flight_params
-    # params.require(:flight).permit(:outboundpartialdate, :destinationplace, :originplace)
+    params.permit(:outboundpartialdate, :destinationplace, :originplace)
   end
 
   def fetch_flights
-    response = Unirest.get "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browseroutes/v1.0/US/USD/en-US/SFO-sky/ORD-sky/2020-09-21",
+    params["flight"].class == NilClass ? date = "#{Date.today}" : date = params["flight"]["outboundpartialdate"]
+    response = Unirest.get "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browseroutes/v1.0/US/USD/en-US/SFO-sky/ORD-sky/#{date}",
     headers:{
       "X-RapidAPI-Host" => "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
       "X-RapidAPI-Key" => "dd277984c2mshb0dba71490fe32fp140ff3jsnbfdc27271a52",
